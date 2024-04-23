d38e0221e6e297ab0351817d9f00448ed212ad8c
EnochWong521 <98492178+EnochWong521@users.noreply.github.com>
2024-04-22 23:42:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,36 +14,11 @@ voltage = [(r / (10000 + r)) * 5 for r in resistance]
 adc_const = 5.0 / int(str(780000), 16)
 adc = [x / adc_const for x in voltage]
 
-plt.figure()
-plt.plot(adc, temp)
-plt.title("Temperature vs. ADC fitted graph")
-plt.xlabel("ADC")
-plt.ylabel("Temperature")
-plt.show()
-
 adc_fit = np.polynomial.polynomial.Polynomial.fit(adc, temp, 7)
-
-plt.figure()
-# plot the original ocv graph
-plt.plot(adc, temp, label="Original graph")
-# plot the fitted ocv graph
-plt.plot(adc, adc_fit(np.array(adc)), label="Fitted graph")
-plt.legend(loc="upper right")
-plt.title("Temperature vs. Shifted fitted graph")
-plt.xlabel("ADC")
-plt.ylabel("Temperature")
-plt.show()
 
 step_size = int((adc[0] - adc[-1]) / 1023)
 fitted_adc = [i for i in range(int(adc[0]), int(adc[-1]), -1 * step_size)]
-fitted_temp = [adc_fit(a) for a in fitted_adc]
-
-plt.figure()
-plt.plot(fitted_adc, fitted_temp)
-plt.title("Temperature vs. ADC graph (final)")
-plt.xlabel("ADC")
-plt.ylabel("Temperature")
-plt.show()
+fitted_temp = [round(adc_fit(a) * 10) for a in fitted_adc]
 
 # shift adc to start at 0
 shifted_adc = [a - fitted_adc[-1] for a in fitted_adc]
@@ -60,3 +35,14 @@ data = {
 df = pd.DataFrame(data)
 
 df.to_csv("adc_to_temp_table.csv", index=False)
+
+
+def get_adc_idx(input_adc):
+    idx = int((input_adc - adc[-1] + (step_size / 2)) / step_size)
+    return fitted_temp[idx]
+
+
+print(f"Step_size: {step_size}")
+print(f"Offset: {adc[-1]}")
+print(f"uint16_t temps[{len(fitted_temp)}] = {fitted_temp};")
+
